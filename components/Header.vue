c14577316275086a5710315ab150ebec44c6dbe3
hungkieutien <hungkieu@web.de>
2024-10-14 18:00:53 +0200

    modules, components, layouts etc. hinzugefügt

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <nav class="navbar">
+      <div class="navbar__logo">
+        <NuxtLink to="/">
+          <div>ETHKL</div>
+        </NuxtLink>
+      </div>
+      <ul class="navbar__links">
+        <li><NuxtLink to="">Shop</NuxtLink></li>
+        <li><NuxtLink to="">About</NuxtLink></li>
+        <li><NuxtLink to="">Contact</NuxtLink></li>
+      </ul>
+      <div class="navbar__icons">
+        <NuxtLink to="">
+          <Icon name="mdi-magnify" />
+        </NuxtLink>
+        <NuxtLink to="">
+          <Icon name="mdi-cart" />
+        </NuxtLink>
+        <NuxtLink to="">
+          <Icon name="mdi-account" />
+        </NuxtLink>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss">
+
+</style>/
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  css: [
+    '@/assets/main.scss'
+  ],
+  modules: ['@nuxtjs/google-fonts', '@nuxt/icon', '@nuxt/image'],
+  googleFonts: {
+    families: {
+      Montserrat: [100, 200, 300, 400, 500, 600, 700],
+    },
+    display: 'swap',
+    prefetch: true,
+    preconnect: true
+  },
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler'
+        }
+      }
+    }
+  },
+
+  compatibilityDate: '2024-10-14'
 })
