1d19644d4670c3c9eff12096c4723c56e75a3fa7
hungkieutien <hungkieu@web.de>
2024-11-07 16:47:24 +0100

    quick fix

--- components/common/Button.vue
+++ components/common/Button.vue
@@ -12,16 +12,14 @@ defineProps<{
 
 <style lang="scss">
 .button-link {
-  .button-link {
-    background-color: #fff;
-    border: 1px solid #000;
-    padding: 0.625rem 1.25rem;
-    font-size: 0.75rem;
-    border-radius: 8px;
-    cursor: pointer;
-    text-transform: uppercase;
-    text-decoration: none;
-    color: #000;
-  }
+  background-color: #fff;
+  border: 0.0625rem solid #000;
+  padding: 0.625rem 1.25rem;
+  font-size: 0.75rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: #000;
 }
 </style>
